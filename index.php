2d6632c6e5a838c6f6c48ab36b8e66065d2e14c2
mrhiraru <mrhiraru@gmail.com>
2023-11-01 17:34:52 +0800

    All Files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    include_once("dbConnection/mysqlconfig_connection.php");
+    include_once("function/fetch.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample CRUD</title>
+</head>
+<body>
+    <h1>My Subjects</h1>
+    <a href="forms/addform.php">Add Subject</a><br/><br/>
+    <table width='100%' border= 1></table>
+</body>
+</html>
